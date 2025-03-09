7658bfbf1fe0afa8dac5b99c48d12c006876735d
yukimasa <120102058+yukimasa0705@users.noreply.github.com>
2025-03-09 20:22:34 +0900

    v1.3.0
    v1.2.0
    プレイヤーの手が入力される際の例外処理発生時にもう一度プレイヤーが手を入力できるように修正しました。
    表示されるメッセージを修正しました。
    v1.3.0
    引き分けの際、もう一度プレイヤーが手を入力できるように修正しました。

--- janken_2.py
+++ janken_2.py
@@ -4,33 +4,38 @@ from random import randint
 print("じゃんけんゲームを始めます")
 # ルールの説明をします
 print("0:グー 1:チョキ 2:パー")
-# プレイヤーの手の入力が例外処理となった場合のループ処理
+# あいこの時、再度手の入力がされるためのループ処理
 while True:
-# プレイヤーの手を入力
-    player_hand=input("プレイヤーの手をルールに従い入力してください>>")
-    jyanken_t="グー","チョキ","パー"
-    # 整数かどうか
-    try:      
-        #　整数に変換
-        player_hand=int(player_hand)
-        #　プレイヤーの手を表示
-        print(f"あなたの手は{jyanken_t[player_hand]}です")
-    except ValueError:
-        print("整数ではない文字が入力されました")
+    # プレイヤーの手の入力が例外処理となった場合のループ処理
+    while True:
+    # プレイヤーの手を入力
+        player_hand=input("プレイヤーの手をルールに従い入力してください>>")
+        jyanken_t="グー","チョキ","パー"
+        # 整数かどうか
+        try:      
+            #　整数に変換
+            player_hand=int(player_hand)
+            #　プレイヤーの手を表示
+            print(f"あなたの手は{jyanken_t[player_hand]}です")
+        except ValueError:
+            print("整数ではない文字が入力されました")
+            continue
+        except IndexError:
+            print("3以上の入力がされました")
+            continue
+        if player_hand<0:
+             print("マイナスの整数が入力がされました")
+             continue
+        break
+    # PCの手を決定
+    PC_hand=randint(0,2)
+    # PCの手を表示
+    print(f"PCの手は{jyanken_t[PC_hand]}です")
+    # 判定
+    result=(player_hand-PC_hand+3)%3
+    # 結果の表示
+    judgement="あいこでしょ","あなたの負けです","あなたの勝ちです"
+    print(judgement[result])
+    if judgement[result]=="あいこでしょ":
         continue
-    except IndexError:
-        print("3以上の入力がされました")
-        continue
-    if player_hand<0:
-         print("マイナスの整数が入力がされました")
-         continue
     break
-# PCの手を決定
-PC_hand=randint(0,2)
-# PCの手を表示
-print(f"PCの手は{jyanken_t[PC_hand]}です")
-# 判定
-result=(player_hand-PC_hand+3)%3
-# 結果の表示
-judgement="引き分けです","あなたの負けです","あなたの勝ちです"
-print(judgement[result])
