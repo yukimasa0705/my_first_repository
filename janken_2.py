5b05ce0c0030e41ef56ab51fe4d7bdb952e56456
yukimasa <120102058+yukimasa0705@users.noreply.github.com>
2025-03-10 10:04:25 +0900

    v1.4.0
    if文をtry-except文に組み込むことでwhileのネストを解消しました。
    エラーハンドリングの部分をすっきりさせ、可読性を高めました

--- janken_2.py
+++ janken_2.py
@@ -6,27 +6,25 @@ print("じゃんけんゲームを始めます")
 print("0:グー 1:チョキ 2:パー")
 # あいこの時、再度手の入力がされるためのループ処理
 while True:
-    # プレイヤーの手の入力が例外処理となった場合のループ処理
-    while True:
     # プレイヤーの手を入力
-        player_hand=input("プレイヤーの手をルールに従い入力してください>>")
-        jyanken_t="グー","チョキ","パー"
-        # 整数かどうか
-        try:      
-            #　整数に変換
-            player_hand=int(player_hand)
-            #　プレイヤーの手を表示
-            print(f"あなたの手は{jyanken_t[player_hand]}です")
-        except ValueError:
-            print("整数ではない文字が入力されました")
+    player_hand=input("プレイヤーの手をルールに従い入力してください>>")
+    jyanken_t="グー","チョキ","パー"
+    # プレイヤーの手の入力が例外処理となった場合のループ処理
+    try:      
+        #　整数に変換
+        player_hand=int(player_hand)
+         # 範囲チェック
+        if player_hand < 0 or player_hand > 2:
+            print("0, 1, 2 のいずれかを入力してください")
             continue
-        except IndexError:
-            print("3以上の入力がされました")
-            continue
-        if player_hand<0:
-             print("マイナスの整数が入力がされました")
-             continue
-        break
+        #　プレイヤーの手を表示
+        print(f"あなたの手は{jyanken_t[player_hand]}です")
+    except ValueError:
+        print("整数ではない文字が入力されました")
+        continue
+    except IndexError:
+        print("3以上の入力がされました")
+        continue
     # PCの手を決定
     PC_hand=randint(0,2)
     # PCの手を表示
