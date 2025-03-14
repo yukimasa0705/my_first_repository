8d26cf088b2437a717739159e9b3379a487fcfdd
yukimasa <ymizuno700705@gmail.com>
2025-03-14 20:18:13 +0900

    v2.2 MyPlayerとPCPlayerの親クラスであるPlayerクラスを追加しました

--- janken_V2.py
+++ janken_V2.py
@@ -1,14 +1,21 @@
-import sys
 # ランダム選択のためにchoiceをインポート
-from random import choice  
-
-# MyPlayerのクラス化（プレイヤークラス作成の布石として名前を変更しました）
-class MyPlayer:
-    jyanken_t = ("グー", "チョキ", "パー")
-
-    def __init__(self, name="プレイヤー"):
-        self.name = name
-
+from random import choice # Playerのクラス化（MyPlayer,PCPlayerの親クラス)
+class Player:
+    # constructer
+    def __init__(self,name):
+        # property
+        self.name=name #my or PC
+        self.hand=None
+        self.jyanken_t = ("グー", "チョキ", "パー")
+    # 手の表示の共通化
+    def show_hand(self):
+        print(f"{self.name}の手は{self.hand}です")
+        return self.hand
+# MyPlayerのクラス化（プレイヤークラス作成の布石として名前を変更しました)       
+class MyPlayer(Player):
+    def __init__(self):
+        # スーパークラス（親クラス）のコンストラクタを呼び出す
+        super().__init__("あなた")
     # get_player_handの関数化
     def get_hand(self):
         # プレイヤーの手を入力して取得する
@@ -19,24 +26,23 @@ class MyPlayer:
                 if int_player_hand < 0 or int_player_hand > 2:
                     print("0, 1, 2 のいずれかを入力してください")
                     continue  # 範囲外なら再入力
-                player_hand = self.jyanken_t[int_player_hand]
-                return player_hand  # グーチョキパーで返す
+                # 親クラスの変数を継承
+                return self.jyanken_t[int_player_hand] # グーチョキパーで返す
             except ValueError:
                 print("整数ではない文字が入力されました")
                 continue  # 無効な入力の場合、再入力
 
 # PCPlayerのクラス化
-class PCPlayer:
-    jyanken_t = ("グー", "チョキ", "パー")
-    def __init__(self, name="PC"):
-        self.name = name
+class PCPlayer(Player): 
+    def __init__(self):
+        # スーパークラス（親クラス）のコンストラクタを呼び出す
+        super().__init__("PC")
 
     # get_pc_handの関数化
     def get_hand(self):
         # PCの手をランダムに取得する
-        pc_hand = choice(self.jyanken_t)
-        return pc_hand
-
+        # 親クラスの変数を継承
+        return choice(self.jyanken_t)
 # judgeのクラス化
 class Judge:
     @staticmethod
@@ -57,13 +63,13 @@ class Judge:
 # じゃんけんゲームのメイン処理
 print("じゃんけんゲームを始めます")
 print("0:グー 1:チョキ 2:パー")
-MyPlayer = MyPlayer()
-PCPlayer = PCPlayer()
+myPlayer = MyPlayer()
+pcPlayer = PCPlayer()
 judge = Judge()
 
 while True:
-    player_hand = MyPlayer.get_hand()
-    pc_hand = PCPlayer.get_hand()
+    player_hand = myPlayer.get_hand()
+    pc_hand = pcPlayer.get_hand()
     print(f"あなたの手は{player_hand}です")
     print(f"PCの手は{pc_hand}です")
 
