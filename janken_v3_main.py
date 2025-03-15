6c22eed25917d60c4bd2cba6950b23714d57b4ae
yukimasa <ymizuno700705@gmail.com>
2025-03-15 15:40:38 +0900

    v3.2 PCに名前を付け、どのPCが勝ったかを明確にしました。 最後の一人が勝ち残るまで、じゃんけんを繰り返すことにしました。

--- janken_v3_main.py
+++ janken_v3_main.py
@@ -6,23 +6,26 @@ print("じゃんけんゲームを始めます")
 print("0:グー 1:チョキ 2:パー")
 # プレイヤーの作成
 myPlayer = MyPlayer()
-pcPlayer1 = PCPlayer()
-pcPlayer2 = PCPlayer()
+pcPlayer1 = PCPlayer(0)
+pcPlayer2 = PCPlayer(1)
 judge = Judge()
 
 # プレイヤーの手を決定
 players = [myPlayer, pcPlayer1, pcPlayer2]
 
-while True:
+while len(players) > 1:
+    while True:
     # それぞれの手を決定
-    for p in players:
-        p.hand=p.get_hand() # ここで手を決める
-        p.show_hand() # 手を表示
+        for p in players:
+            p.hand=p.get_hand() # ここで手を決める
+            p.show_hand() # 手を表示
 
-    # 勝者を判定
-    winners = Judge.judge(players)
-    # 引き分けでもう一度繰り返す
-    if winners:
-        break
-    else:
-        continue
+        # 勝者を判定
+        winners = Judge.judge(players)
+        # 引き分けでもう一度繰り返す
+        if winners:
+            players = winners
+            break
+        else:
+            continue
+print(f"最終勝者は {players[0].name} です！")
